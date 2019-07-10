5ca46c94b8b587badacbdb098a9340cb094a3382
Fil Zembowicz <fil@filosophy.org>
2019-07-10 15:49:28 -0400

    Add constants for responder uuid

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -6,4 +6,7 @@ export const SET_ANSWER_VALUE_MSG = 'ƒ_sav';
 export const REQUEST_ANSWERS_MSG = 'ƒ_ra';
 export const SET_ANSWERS_MSG = 'ƒ_sa';
 
+export const REQUEST_RESPONDER_UUID_MSG = 'ƒ_rru';
+export const SET_RESPONDER_UUID_MSG = 'ƒ_sru';
+
 export const CLEAR_ANSWER_VALUE_MSG = 'ƒ_cav';
