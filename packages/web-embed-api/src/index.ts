2edf5775f64d7cb631c97e944605ffe1b473c3ed
Fil Zembowicz <fil@formsort.com>
2019-11-21 18:12:20 -0500

    Move away from classes

--- packages/web-embed-api/src/index.ts
+++ packages/web-embed-api/src/index.ts
@@ -7,57 +7,67 @@ import {
 const FS_ORIGIN = (window as any).FS_ORIGIN;
 const FLOW_ORIGIN = FS_ORIGIN || `https://flow.formsort.com`;
 
+interface IFormsortWebEmbed {
+  loadFlow: (
+    clientLabel: string,
+    flowLabel: string,
+    variantLabel?: string,
+    queryParams?: Array<[string, string]>
+  ) => void;
+  setSize: (width: string, height: string) => void;
+  onFlowLoaded: () => void;
+  onFlowClosed: () => void;
+  onFlowFinalized: () => void;
+  onRedirect: (url: string) => void;
+}
+
 interface IFormsortWebEmbedConfig {
   useHistoryAPI: boolean;
 }
 const DEFAULT_CONFIG: IFormsortWebEmbedConfig = { useHistoryAPI: false };
 
-class FormsortWebEmbed {
-  private rootEl: HTMLElement;
-  private iframeEl: HTMLIFrameElement;
-  private onFlowLoadedCallback?: () => void;
-  private onFlowClosedCallback?: () => void;
-  private onFlowFinalizedCallback?: () => void;
-  private onRedirectCallback?: (url: string) => void;
-  private config: IFormsortWebEmbedConfig;
+const FormsortWebEmbed = (
+  rootEl: HTMLElement,
+  config: IFormsortWebEmbedConfig = DEFAULT_CONFIG
+): IFormsortWebEmbed => {
+  const iframeEl = document.createElement('iframe');
+  iframeEl.style.border = 'none';
 
-  constructor(
-    rootEl: HTMLElement,
-    config: IFormsortWebEmbedConfig = DEFAULT_CONFIG
-  ) {
-    this.rootEl = rootEl;
-    this.config = config;
-    const iframeEl = document.createElement('iframe');
-    iframeEl.style.border = 'none';
-    this.iframeEl = iframeEl;
-    rootEl.appendChild(iframeEl);
-    window.addEventListener('message', this.onWindowMessage);
-  }
+  rootEl.appendChild(iframeEl);
 
-  setSize = (width: string, height: string) => {
-    this.iframeEl.style.width = width;
-    this.iframeEl.style.height = height;
+  const onRedirectMessage = (redirectData: IIFrameRedirectEventData) => {
+    const currentUrl = window.location.href;
+    const currentHash = window.location.hash.slice(1);
+    const currentUrlBase = currentUrl.replace(currentHash, '');
+
+    const url = redirectData.payload;
+
+    if (onRedirectCallback) {
+      onRedirectCallback(url);
+      return;
+    }
+
+    const hashIndex = url.indexOf('#');
+    const urlHash = hashIndex >= 0 ? url.slice(hashIndex + 1) : undefined;
+    const urlBase = urlHash !== undefined ? url.replace(urlHash, '') : url;
+
+    if (urlHash && urlBase === currentUrlBase && urlHash !== currentHash) {
+      window.location.hash = urlHash;
+    }
+    if (
+      config.useHistoryAPI &&
+      'history' in window &&
+      url.indexOf(window.location.origin) === 0
+    ) {
+      window.history.pushState({}, document.title, url);
+    } else {
+      window.location.assign(url);
+    }
   };
 
-  set onFlowLoaded(callback: () => void) {
-    this.onFlowLoadedCallback = callback;
-  }
-
-  set onFlowClosed(callback: () => void) {
-    this.onFlowClosedCallback = callback;
-  }
-
-  set onFlowFinalized(callback: () => void) {
-    this.onFlowFinalizedCallback = callback;
-  }
-
-  set onRedirect(callback: (url: string) => void) {
-    this.onRedirectCallback = callback;
-  }
-
-  onWindowMessage = (message: MessageEvent) => {
+  const onWindowMessage = (message: MessageEvent) => {
     const { origin, source, data } = message;
-    if (source !== this.iframeEl.contentWindow) {
+    if (source !== iframeEl.contentWindow) {
       // If we have multiple formsorts within a page, only listen to events coming
       // from the iframe that this embed instance controls.
       return;
@@ -72,66 +82,47 @@ class FormsortWebEmbed {
     }
 
     if (data.type === WebEmbedMessage.EMBED_EVENT_MSG) {
-      this.onEventMessage(data as IIFrameAnalyticsEventData);
+      onEventMessage(data as IIFrameAnalyticsEventData);
     } else if (data.type === WebEmbedMessage.EMBED_REDIRECT_MSG) {
-      this.onRedirectMessage(data as IIFrameRedirectEventData);
+      onRedirectMessage(data as IIFrameRedirectEventData);
     }
   };
+  window.addEventListener('message', onWindowMessage);
 
-  onEventMessage = (eventData: IIFrameAnalyticsEventData) => {
+  let onFlowLoadedCallback: () => void;
+  let onFlowClosedCallback: () => void;
+  let onFlowFinalizedCallback: () => void;
+  let onRedirectCallback: (url: string) => void;
+
+  const setSize = (width: string, height: string) => {
+    iframeEl.style.width = width;
+    iframeEl.style.height = height;
+  };
+
+  const onEventMessage = (eventData: IIFrameAnalyticsEventData) => {
     const { eventType } = eventData;
     if (eventType === AnalyticsEventType.FlowLoaded) {
-      if (this.onFlowLoadedCallback) {
-        this.onFlowLoadedCallback();
+      if (onFlowLoadedCallback) {
+        onFlowLoadedCallback();
       }
     } else if (eventType === AnalyticsEventType.FlowClosed) {
-      this.removeListeners();
-      this.rootEl.removeChild(this.iframeEl);
-      if (this.onFlowClosedCallback) {
-        this.onFlowClosedCallback();
+      removeListeners();
+      rootEl.removeChild(iframeEl);
+      if (onFlowClosedCallback) {
+        onFlowClosedCallback();
       }
     } else if (eventType === AnalyticsEventType.FlowFinalized) {
-      if (this.onFlowFinalizedCallback) {
-        this.onFlowFinalizedCallback();
+      if (onFlowFinalizedCallback) {
+        onFlowFinalizedCallback();
       }
     }
   };
 
-  onRedirectMessage = (redirectData: IIFrameRedirectEventData) => {
-    const currentUrl = window.location.href;
-    const currentHash = window.location.hash.slice(1);
-    const currentUrlBase = currentUrl.replace(currentHash, '');
-
-    const url = redirectData.payload;
-
-    if (this.onRedirectCallback) {
-      this.onRedirectCallback(url);
-      return;
-    }
-
-    const hashIndex = url.indexOf('#');
-    const urlHash = hashIndex >= 0 ? url.slice(hashIndex + 1) : undefined;
-    const urlBase = urlHash !== undefined ? url.replace(urlHash, '') : url;
-
-    if (urlHash && urlBase === currentUrlBase && urlHash !== currentHash) {
-      window.location.hash = urlHash;
-    }
-    if (
-      this.config.useHistoryAPI &&
-      'history' in window &&
-      url.indexOf(window.location.origin) === 0
-    ) {
-      window.history.pushState({}, document.title, url);
-    } else {
-      window.location.assign(url);
-    }
+  const removeListeners = () => {
+    window.removeEventListener('message', onWindowMessage);
   };
 
-  removeListeners = () => {
-    window.removeEventListener('message', this.onWindowMessage);
-  };
-
-  loadFlow = (
+  const loadFlow = (
     clientLabel: string,
     flowLabel: string,
     variantLabel?: string,
@@ -146,8 +137,25 @@ class FormsortWebEmbed {
         .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
         .join('&')}`;
     }
-    this.iframeEl.src = url;
+    iframeEl.src = url;
   };
-}
+
+  return {
+    loadFlow,
+    setSize,
+    set onFlowLoaded(callback: () => void) {
+      onFlowLoadedCallback = callback;
+    },
+    set onFlowClosed(callback: () => void) {
+      onFlowClosedCallback = callback;
+    },
+    set onFlowFinalized(callback: () => void) {
+      onFlowFinalizedCallback = callback;
+    },
+    set onRedirect(callback: (url: string) => void) {
+      onRedirectCallback = callback;
+    },
+  };
+};
 
 export default FormsortWebEmbed;
