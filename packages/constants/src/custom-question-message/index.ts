6c508ff04ae7ca1448c77353d8626db2fbdd3129
Fil Zembowicz <fil@formsort.com>
2020-03-23 11:18:48 -0400

    Add events for get/set semantic answer values
    Also bump dependencies

--- packages/constants/src/custom-question-message/index.ts
+++ packages/constants/src/custom-question-message/index.ts
@@ -2,6 +2,8 @@ enum CustomQuestionMessage {
   SET_QUESTION_SIZE_MSG = 'ƒ_sqz',
   REQUEST_ANSWER_VALUE_MSG = 'ƒ_rav',
   SET_ANSWER_VALUE_MSG = 'ƒ_sav',
+  REQUEST_SEMANTIC_ANSWER_VALUE_MSG = 'ƒ_rsv',
+  SET_SEMANTIC_ANSWER_VALUE_MSG = 'ƒ_ssv',
   REQUEST_ANSWERS_MSG = 'ƒ_ra',
   SET_ANSWERS_MSG = 'ƒ_sa',
   REQUEST_RESPONDER_UUID_MSG = 'ƒ_rru',
