d5befd5c7837003dea1775631a9f814bc36a15cd
Fil Zembowicz <fil@formsort.com>
2021-08-26 11:33:53 -0400

    Add responder_user_id to constants

--- packages/constants/src/semantic-type/index.ts
+++ packages/constants/src/semantic-type/index.ts
@@ -1,4 +1,4 @@
-type AnswerSemanticType = 
+type AnswerSemanticType =
   | 'responder_email'
   | 'responder_first_name'
   | 'responder_last_name'
@@ -6,6 +6,7 @@ type AnswerSemanticType =
   | 'responder_phone'
   | 'responder_dob'
   | 'responder_mailing_address'
+  | 'responder_user_id'
   | 'responder_other_pii';
 
 export default AnswerSemanticType;
