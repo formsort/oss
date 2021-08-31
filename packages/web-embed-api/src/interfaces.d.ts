973f8e849ef9d3b7cdf4fd0d0bc33a336ced6894
lizraeli <leo2002b@yahoo.com>
2021-08-31 08:13:38 -0400

    update interfaces

--- packages/web-embed-api/src/interfaces.d.ts
+++ packages/web-embed-api/src/interfaces.d.ts
@@ -1,20 +1,53 @@
 import { AnalyticsEventType, WebEmbedMessage } from '@formsort/constants';
 
-export interface IIFrameAnalyticsEventData {
+/**
+ * @TODO: these types should be moved to @formsort/constants
+ * And used by both formsort/web and web-embed-api
+ */
+
+export type JSONPrimitive = string | boolean | number;
+
+export interface IAddressObject {
+  raw?: string;
+  address_1: string;
+  address_2?: string;
+  city: string;
+  state: string;
+  postal_code: string;
+  country?: string;
+}
+
+export type IAnswer =
+  | undefined
+  | JSONPrimitive
+  | JSONPrimitive[]
+  | Partial<IAddressObject>;
+
+export interface IFlowAnswers {
+  [answerKey: string]: IAnswer;
+}
+
+interface WebEmbedEventData  {
+  type: WebEmbedMessage;
+}
+
+export interface IIFrameAnalyticsEventData extends WebEmbedEventData {
   createdAt: Date;
   eventType: AnalyticsEventType;
   type: WebEmbedMessage.EMBED_EVENT_MSG;
+  // Answers are only available when the iframe is embedded in a whitelisted domain
+  answers: IFlowAnswers | undefined;
 }
 
-export interface IIFrameRedirectEventData {
+export interface IIFrameRedirectEventData extends WebEmbedEventData {
   payload: string;
-  type: WebEmbedMessage.EMBED_REDIRECT_MSG;
+  type: WebEmbedMessage.EMBED_REDIRECT_MSG
 }
 
-export interface IIFrameResizeEventData {
+export interface IIFrameResizeEventData extends WebEmbedEventData {
   payload: {
     width?: number;
     height?: number;
   };
-  type: WebEmbedMessage.EMBED_RESIZE_MSG;
+  type: WebEmbedMessage.EMBED_REDIRECT_MSG
 }
