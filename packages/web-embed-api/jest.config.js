afbf5064ae28c9f3ab0b84a737c7ab4f7010751c
Burak Yigit Kaya <ben@byk.im>
2022-03-10 12:09:55 +0300

    add jest cache, workdir fix

--- packages/web-embed-api/jest.config.js
+++ packages/web-embed-api/jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
+  cacheDirectory: './.jest-cache',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  "testEnvironment": "jsdom"
-}
+  testEnvironment: 'jsdom',
+};
