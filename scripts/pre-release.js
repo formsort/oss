4948a529e20545165f7038da843a8932f18965b3
Burak Yigit Kaya <byk@formsort.com>
2022-04-27 16:28:24 +0300

    ci: Some more simplifications
    #skip-changelog

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -1,12 +1,11 @@
 const { execSync } = require('child_process');
-console.log(
-  execSync(
-    [
-      'yarn',
-      '--no-git-tag-version',
-      'version',
-      '--new-version',
-      process.env.CRAFT_NEW_VERSION,
-    ].join(' ')
-  ).toString()
+execSync(
+  [
+    'yarn',
+    '--no-git-tag-version',
+    'version',
+    '--new-version',
+    process.env.CRAFT_NEW_VERSION,
+  ].join(' '),
+  { stdio: 'inherit' }
 );
