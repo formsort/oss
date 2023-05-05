77f50a996baca60af4b2e20aabe66933fd3f1b47
Ali Kireçligöl <alikirecligol@gmail.com>
2023-05-05 16:00:58 +0300

    FLOW-605: move web-embed messaging logic to a class

--- packages/web-embed-api/src/index.ts
+++ packages/web-embed-api/src/index.ts
@@ -1,28 +1,12 @@
-import {
-  AnalyticsEventType,
-  IIFrameAnalyticsEventData,
-  IIFrameRedirectEventData,
-  IIFrameResizeEventData,
-  IIFrameTokenRequestEventData,
-  IFlowAnswers,
-  TokenRequestPayload,
-  WebEmbedMessage,
-} from '@formsort/constants';
+import EmbedMessagingManager, {
+  type IFormsortEmbedConfig,
+  type IEventMap,
+} from './EmbedMessagingManager';
 import { getMessageSender } from './iframe-utils';
-import {
-  isIWebEmbedEventData,
-  isIFrameRedirectEventData,
-  isIFrameResizeEventData,
-  isIFrameAnalyticsEventData,
-  isIFrameStyleSetRequestEventData,
-  isIFrameTokenRequestEventData,
-  isIFrameUnauthorizedEventData,
-} from './typeGuards';
-import { addToArrayMap, isEmpty, removeFromArrayMap } from './utils';
 
-const DEFAULT_FLOW_ORIGIN = `https://flow.formsort.com`;
+const DEFAULT_FLOW_ORIGIN = 'https://flow.formsort.com';
 
-export interface IFormsortWebEmbed {
+interface IFormsortWebEmbed {
   loadFlow: (
     clientLabel: string,
     flowLabel: string,
@@ -41,20 +25,8 @@ export interface IFormsortWebEmbed {
   ): void;
 }
 
-interface IAuthenticationConfig {
-  idToken: string;
-}
-
-export interface IFormsortWebEmbedConfig {
+interface IFormsortWebEmbedConfig extends IFormsortEmbedConfig {
   useHistoryAPI?: boolean;
-  autoHeight?: boolean;
-  style?: Partial<Pick<CSSStyleDeclaration, 'width' | 'height'>>;
-  /**
-   * For Formsort internal use only
-   */
-  styleSet?: Record<string, unknown>;
-  origin?: string;
-  authentication?: IAuthenticationConfig;
 }
 
 const DEFAULT_CONFIG: IFormsortWebEmbedConfig = {
@@ -62,51 +34,12 @@ const DEFAULT_CONFIG: IFormsortWebEmbedConfig = {
   origin: DEFAULT_FLOW_ORIGIN,
 };
 
-export enum SupportedAnalyticsEvent {
-  FlowLoaded = AnalyticsEventType.FlowLoaded,
-  FlowClosed = AnalyticsEventType.FlowClosed,
-  FlowFinalized = AnalyticsEventType.FlowFinalized,
-  StepLoaded = AnalyticsEventType.StepLoaded,
-  StepCompleted = AnalyticsEventType.StepCompleted,
-  ResponderStateUpdated = AnalyticsEventType.ResponderStateUpdated,
-}
-
-export const isSupportedEventType = (
-  eventType: AnalyticsEventType | SupportedAnalyticsEvent
-): eventType is SupportedAnalyticsEvent => eventType in SupportedAnalyticsEvent;
-
-interface IBaseEventData {
-  answers: IFlowAnswers | undefined;
-}
-
-interface IRedirectEventData extends IBaseEventData {
-  url: string;
-}
-
-export type IEventListener = (props: IBaseEventData) => void;
-
-export type IAnalyticsEventMap = Record<
-  SupportedAnalyticsEvent,
-  IEventListener
->;
-
-export interface IEventMap extends IAnalyticsEventMap {
-  redirect: (props: IRedirectEventData) => {
-    cancel?: boolean;
-  } | void;
-  unauthorized: () => void;
-}
-
-export type IEventListenersArrayMap = {
-  [K in keyof IEventMap]: Array<IEventMap[K]>;
-};
-
 const FormsortWebEmbed = (
   rootEl: HTMLElement,
   config: IFormsortWebEmbedConfig = DEFAULT_CONFIG
 ): IFormsortWebEmbed => {
   const iframeEl = document.createElement('iframe');
-  const { style, autoHeight } = config;
+  const { style } = config;
   const formsortOrigin = config.origin || DEFAULT_FLOW_ORIGIN;
   iframeEl.style.border = 'none';
   if (style) {
@@ -116,84 +49,38 @@ const FormsortWebEmbed = (
   }
 
   rootEl.appendChild(iframeEl);
-  const sendMessage = getMessageSender(iframeEl);
 
-  const eventListenersArrayMap: IEventListenersArrayMap = {
-    [SupportedAnalyticsEvent.FlowLoaded]: [],
-    [SupportedAnalyticsEvent.FlowClosed]: [],
-    [SupportedAnalyticsEvent.FlowFinalized]: [],
-    [SupportedAnalyticsEvent.StepLoaded]: [],
-    [SupportedAnalyticsEvent.StepCompleted]: [],
-    [SupportedAnalyticsEvent.ResponderStateUpdated]: [],
-    redirect: [],
-    unauthorized: [],
+  const setSize = (width?: string | number, height?: string | number) => {
+    if (width !== undefined) {
+      iframeEl.style.width = width.toString();
+    }
+    if (height !== undefined) {
+      iframeEl.style.height = height.toString();
+    }
   };
 
-  const onStyleSetRequest = () => {
-    sendMessage({
-      type: WebEmbedMessage.EMBED_STYLE_SET_RESPONSE_MSG,
-      payload: { styleSet: config.styleSet },
-    });
+  const unloadFlow = () => {
+    removeListeners();
+    rootEl.removeChild(iframeEl);
   };
 
-  const onTokenRequest = (data: IIFrameTokenRequestEventData) => {
-    const { payload } = data;
-
-    if (payload === TokenRequestPayload.ID) {
-      if (config.authentication?.idToken) {
-        sendMessage({
-          type: WebEmbedMessage.EMBED_TOKEN_RESPONSE_MSG,
-          payload: { token: config.authentication.idToken },
-        });
+  const messagingManager = new EmbedMessagingManager({
+    config,
+    sendMessageToEmbed: getMessageSender(iframeEl),
+    onRedirect: (url: string) => {
+      if (
+        config.useHistoryAPI &&
+        'history' in window &&
+        url.indexOf(window.location.origin) === 0
+      ) {
+        window.history.pushState({}, document.title, url);
       } else {
-        throw new Error(
-          `The loaded Flow requires authentication using an ID token, please provide it in config.authentication.idToken.`
-        );
+        window.location.assign(url);
       }
-    }
-  };
-
-  const onRedirectMessage = (redirectData: IIFrameRedirectEventData) => {
-    const { payload: url, answers } = redirectData;
-
-    if (!isEmpty(eventListenersArrayMap.redirect)) {
-      let cancelRedirect = false;
-      // Cancel redirect if any of the redirect listeners return `{ cancel: true }`
-      for (const redirectListener of eventListenersArrayMap.redirect) {
-        const { cancel } = redirectListener({ url, answers }) || {};
-        if (!cancelRedirect && cancel) {
-          cancelRedirect = true;
-        }
-      }
-
-      if (cancelRedirect) {
-        return;
-      }
-    }
-
-    if (
-      config.useHistoryAPI &&
-      'history' in window &&
-      url.indexOf(window.location.origin) === 0
-    ) {
-      window.history.pushState({}, document.title, url);
-    } else {
-      window.location.assign(url);
-    }
-  };
-
-  const onUnauthorizedMessage = () => {
-    if (!isEmpty(eventListenersArrayMap.unauthorized)) {
-      for (const unathorizedListener of eventListenersArrayMap.unauthorized) {
-        unathorizedListener();
-      }
-    }
-  };
-
-  const onResizeMessage = (data: IIFrameResizeEventData) => {
-    const { width, height } = data.payload;
-    setSize(width, height);
-  };
+    },
+    onResize: setSize,
+    onFlowClosed: unloadFlow,
+  });
 
   const onWindowMessage = (message: MessageEvent<unknown>) => {
     const { origin: msgOrigin, source, data } = message;
@@ -207,66 +94,12 @@ const FormsortWebEmbed = (
       return;
     }
 
-    if (!isIWebEmbedEventData(data)) {
-      return;
-    }
-
-    if (isIFrameAnalyticsEventData(data)) {
-      onEventMessage(data);
-    } else if (isIFrameTokenRequestEventData(data)) {
-      onTokenRequest(data);
-    } else if (isIFrameRedirectEventData(data)) {
-      onRedirectMessage(data);
-    } else if (isIFrameResizeEventData(data) && autoHeight) {
-      onResizeMessage(data);
-    } else if (isIFrameUnauthorizedEventData(data)) {
-      onUnauthorizedMessage();
-    } else if (isIFrameStyleSetRequestEventData(data)) {
-      onStyleSetRequest();
-    }
+    messagingManager.onMessage(data);
   };
 
   if (typeof window !== undefined) {
     window.addEventListener('message', onWindowMessage);
   }
-
-  const setSize = (width?: string | number, height?: string | number) => {
-    if (width !== undefined) {
-      iframeEl.style.width = width.toString();
-    }
-    if (height !== undefined) {
-      iframeEl.style.height = height.toString();
-    }
-  };
-
-  const getEventListenerArray = (
-    eventType: AnalyticsEventType
-  ): IEventListener[] | undefined => {
-    if (isSupportedEventType(eventType)) {
-      return eventListenersArrayMap[eventType];
-    }
-
-    return undefined;
-  };
-
-  const onEventMessage = (eventData: IIFrameAnalyticsEventData) => {
-    const { eventType, answers } = eventData;
-
-    if (eventType === AnalyticsEventType.FlowClosed) {
-      removeListeners();
-      rootEl.removeChild(iframeEl);
-    }
-
-    const eventListenersArr = getEventListenerArray(eventType);
-
-    if (!eventListenersArr) {
-      return;
-    }
-
-    for (const eventListener of eventListenersArr) {
-      eventListener({ answers });
-    }
-  };
 
   const removeListeners = () => {
     window.removeEventListener('message', onWindowMessage);
@@ -293,28 +126,22 @@ const FormsortWebEmbed = (
     iframeEl.src = url;
   };
 
-  const unloadFlow = () => {
-    removeListeners();
-    rootEl.removeChild(iframeEl);
-  };
-
   return {
     loadFlow,
     unloadFlow,
     setSize,
-    addEventListener<K extends keyof IEventMap>(
-      eventName: K,
-      fn: IEventMap[K]
-    ): void {
-      addToArrayMap(eventListenersArrayMap, eventName, fn);
-    },
-    removeEventListener<K extends keyof IEventMap>(
-      eventName: K,
-      eventListener: IEventMap[K]
-    ): void {
-      removeFromArrayMap(eventListenersArrayMap, eventName, eventListener);
-    },
+    addEventListener: messagingManager.addEventListener,
+    removeEventListener: messagingManager.removeEventListener,
   };
 };
+
+export { IFormsortWebEmbed, IFormsortWebEmbedConfig, IEventMap };
+export {
+  type IAnalyticsEventMap,
+  type IEventListener,
+  type IEventListenersArrayMap,
+  isSupportedEventType,
+  SupportedAnalyticsEvent,
+} from './EmbedMessagingManager'; 
 
 export default FormsortWebEmbed;
